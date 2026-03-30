8c3b69799c92baf407eca6de740129d0ee45c770
anders-ki <anders.norman@ki.se>
2026-03-30 14:48:29 +0200

    Rewrote everything bro

--- scripts/helloworld.R
+++ scripts/helloworld.R
@@ -1,4 +1,10 @@
 #Just a quick script for testing Git
-print('hello world')
-print("hello again")
-Print("just test")
+library("tidyverse")
+df <- read.csv("data/raw/20260330_raw.csv", header = TRUE, )
+names(df) <- c("Age", "Height_cm")
+write.csv(df, "data/processed/processed_df.csv", row.names = FALSE)
+df_proc <- read.csv("data/processed/processed_df.csv", header = TRUE)
+df.plot <- ggplot(df_proc, aes(x = Age, y = Height_cm)) + 
+                 geom_point()
+df.plot <- df.plot + geom_line()
+ggsave('results/20260330_processed_plot.png', df.plot)
